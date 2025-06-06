50f73b7e2a1db3f9c6b652fc2f0e6d027bce092e
Daniel Lima <52128795+ddaniboy@users.noreply.github.com>
2025-06-05 21:39:03 -0300

    adicionando Properties
    Adicionando função para modificação e adição de propriedades no android

---
+++ app/src/main/java/io/hakaisecurity/beerusframework/composables/PropertiesComposable.kt
@@ -0,0 +1,262 @@
+package io.hakaisecurity.beerusframework.composables
+
+import android.content.Context
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import io.hakaisecurity.beerusframework.core.functions.Properties.Properties.removeProperty
+import io.hakaisecurity.beerusframework.core.functions.Properties.Properties.addProperty
+import io.hakaisecurity.beerusframework.core.functions.Properties.Properties.listProperties
+import io.hakaisecurity.beerusframework.core.models.NavigationState.Companion.animationStart
+import io.hakaisecurity.beerusframework.core.models.NavigationState.Companion.updateanimationStartState
+import io.hakaisecurity.beerusframework.core.utils.CommandUtils.Companion.runSuCommand
+import io.hakaisecurity.beerusframework.ui.theme.Trash
+import io.hakaisecurity.beerusframework.ui.theme.ibmFont
+
+@Composable
+fun PropertiesScreen(modifier: Modifier, context: Context) {
+    var newProperyName by remember { mutableStateOf("") }
+    var newProperyValue by remember { mutableStateOf("") }
+
+    val propertiesState = remember { mutableStateOf(listProperties()) }
+
+    val properties = propertiesState.value
+    var showPropertiesDialog by remember { mutableStateOf(false) }
+
+    fun refreshProperties() {
+        propertiesState.value = listProperties()
+    }
+
+    LaunchedEffect(Unit) {
+        refreshProperties()
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .padding(35.dp, 0.dp)
+            .fillMaxSize()
+    ) {
+        Row () {
+            Button(
+                onClick = {
+                    if(!animationStart) {
+                        showPropertiesDialog = true
+                    } else {
+                        updateanimationStartState(false)
+                    }
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White
+                ),
+                shape = RoundedCornerShape(10.dp),
+                modifier = modifier
+                    .padding(end = 5.dp)
+                    .width(140.dp)
+            ) {
+                Text(
+                    text = "Add Property",
+                    fontSize = 11.sp,
+                    color = Color.Red,
+                    modifier = modifier.padding(0.dp, 3.dp),
+                    fontFamily = ibmFont
+                )
+            }
+            Button(
+                onClick = {
+                    runSuCommand("reboot") {}
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White
+                ),
+                shape = RoundedCornerShape(10.dp),
+                modifier = modifier
+                    .padding(end = 5.dp)
+                    .width(140.dp)
+            ) {
+                Text(
+                    text = "Reboot",
+                    fontSize = 11.sp,
+                    color = Color.Red,
+                    modifier = modifier.padding(0.dp, 3.dp),
+                    fontFamily = ibmFont
+                )
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            properties.forEach { property ->
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(0.dp, 5.dp)
+                        .background(Color(0xFF151515), shape = RoundedCornerShape(4.dp))
+                        .border(width = 2.dp, color = Color.White)
+                ) {
+                    Column {
+                        Column(Modifier.padding(16.dp, 8.dp)) {
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    text = property.name,
+                                    textDecoration = TextDecoration.None,
+                                    color = Color.White,
+                                    fontWeight = FontWeight.Bold,
+                                    fontFamily = ibmFont,
+                                    fontSize = 16.sp,
+                                    modifier = Modifier.weight(1f)
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    text = property.value,
+                                    textDecoration = TextDecoration.None,
+                                    color = Color.White,
+                                    fontWeight = FontWeight.SemiBold,
+                                    fontFamily = ibmFont,
+                                    fontSize = 12.sp
+                                )
+                            }
+                        }
+
+                        Spacer(
+                            modifier = Modifier
+                                .height(2.dp)
+                                .fillMaxWidth()
+                                .background(Color.White)
+                        )
+
+                        Row(
+                            modifier = modifier
+                                .fillMaxWidth()
+                                .padding(16.dp, 10.dp)
+                                .clickable(
+                                    indication = null,
+                                    interactionSource = remember { MutableInteractionSource() }
+                                ) {
+                                    if(!animationStart) {
+                                        removeProperty(property.name)
+                                        refreshProperties()
+                                    }else {
+                                        updateanimationStartState(false)
+                                    }
+                                },
+                            horizontalArrangement = Arrangement.End,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = "Remove",
+                                color = Color.White,
+                                fontWeight = FontWeight.Bold,
+                                fontFamily = ibmFont,
+                                fontSize = 14.sp
+                            )
+                            Icon(
+                                imageVector = Trash,
+                                contentDescription = "Trash",
+                                tint = Color.White,
+                                modifier = Modifier
+                                    .size(24.dp)
+                                    .padding(start = 5.dp)
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if (showPropertiesDialog) {
+
+        AlertDialog(
+            onDismissRequest = { showPropertiesDialog = false },
+            title = { Text("Enter Property details") },
+            text = {
+                Column {
+                    TextField(
+                        value = newProperyName,
+                        onValueChange = { newProperyName = it },
+                        placeholder = { Text("Property name") },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    TextField(
+                        value = newProperyValue,
+                        onValueChange = {
+                            newProperyValue = it
+                        },
+                        placeholder = { Text("Property value") },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            },
+            confirmButton = {
+                TextButton(onClick = {
+                    showPropertiesDialog = false
+                    addProperty(name = newProperyName, value = newProperyValue)
+                    newProperyName = ""
+                    newProperyValue = ""
+                    refreshProperties()
+                }) {
+                    Text("Create")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { showPropertiesDialog = false }) {
+                    Text("Cancel")
+                }
+            }
+        )
+    }
+}
+
---
+++ app/src/main/java/io/hakaisecurity/beerusframework/core/functions/Properties/Properties.kt
@@ -0,0 +1,60 @@
+package io.hakaisecurity.beerusframework.core.functions.Properties
+
+import android.util.Log
+import io.hakaisecurity.beerusframework.core.utils.CommandUtils.Companion.runSuCommand
+import java.io.IOException
+
+object Properties {
+    var systemPropPath = "/data/adb/modules/beerusMagiskModule/system.prop"
+
+    data class PropertyData(
+        val name: String,
+        val value: String
+    )
+
+    fun listProperties(): List<PropertyData> {
+        val latch = java.util.concurrent.CountDownLatch(1)
+
+        return try {
+            val properties = mutableListOf<PropertyData>()
+            runSuCommand("cat $systemPropPath") { content ->
+                val contents = content.split("\n")
+                for (c in contents) {
+                    if (c != "") {
+                        val d = c.split("=", limit=2)
+                        val name = d[0]
+                        val value = d[1]
+                        properties.add(PropertyData(name, value))
+                    }
+                }
+                latch.countDown()
+            }
+            latch.await()
+            properties
+        } catch (e: IOException) {
+            e.printStackTrace()
+            emptyList()
+        }
+    }
+
+    fun addProperty(name: String, value: String) {
+        val latch = java.util.concurrent.CountDownLatch(1)
+        val name = name.replace("\"", "\\\"").replace("'", "\\'")
+        val value = value.replace("\"", "\\\"").replace("'", "\\'")
+
+        runSuCommand("echo \"$name=$value\" >> $systemPropPath") { result ->
+            latch.countDown()
+        }
+        latch.await()
+    }
+
+    fun removeProperty(name: String) {
+        val latch = java.util.concurrent.CountDownLatch(1)
+        val name = name.replace("\"", "\\\"").replace("'", "\\'")
+
+        runSuCommand("prop=\"$name\"; sed -i \"/^\${prop}=/d\" $systemPropPath") { result ->
+            latch.countDown()
+        }
+        latch.await()
+    }
+}
